4e6ba571a6715100e62ad2f88c54770210d9b450
gerges2 <gergesvictor512@gmail.com>
2023-04-03 09:06:54 +0200

    update  position of paddle  and add comment for code and change css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,29 +7,32 @@ document.querySelector(".start-btn span").onclick = function (){
 // Get canvas element
 const canvas = document.getElementById("myCanvas");
 // <<<<<<< HEAD
+// global variable for frams and paddle 
 const fps =60;
-const playerHeight=20;
+const playerHeight=10;
 const playerWidth=100;
 const ctx =canvas.getContext("2d");
-
+// to draw the paddle
 function drawrect(x,y,w,h,color){
     ctx.fillStyle = color;
     ctx.fillRect(x,y,w,h)
 }
+//opject of paddle 
 const player1 ={
         x:canvas.width/2-playerWidth/2,
-        y :canvas.height-10,
+        y :canvas.height-30,
         width:playerWidth,
         height:playerHeight,
         color:"blue",
         score:0
     }
+    //to shows elemet in canvas  
     function rander(){
-    ctx.reset();
-    drawrect(player1.x,player1.y,player1.width,player1.height,player1.color)
+    ctx.reset();// to delete canvas contant
+    drawrect(player1.x,player1.y,player1.width,player1.height,player1.color)// create new canvas by new position
     }
     // rander()
-    function update(){
+    function update(){//to  update position of player(paddle)
             canvas.addEventListener("mousemove",function(e){
             let rect =canvas.getBoundingClientRect();
             if (e.clientX-rect.left<97){}
@@ -37,7 +40,7 @@ const player1 ={
             player1.x=e.clientX-rect.left-(player1.width);}                        
         })//height of player is 100  
     }
-    function game(){
+    function game(){//to display frame by frame 
             update();
             rander();
         }
