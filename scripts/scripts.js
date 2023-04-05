db686a04185aa7092dbdbaeaf862deba66414fb5
al1mmdouh <alimmdouh340@gmail.com>
2023-04-05 09:09:25 +0200

    Merged with main & ( mina + alnahhal )

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,47 +1,29 @@
-
-// // Action for Start Screen
-// // span button
-// document.querySelector(".start-btn span").onclick = function (){
-//     document.querySelector(".start-btn").remove();
-// }
-
+// Action for Start Screen
+// span button
+document.querySelector(".start-btn span").onclick = function (){
+    document.querySelector(".start-btn").remove();
+}// to delete button
 // Get canvas element
 const canvas = document.getElementById("myCanvas");
-// <<<<<<< HEAD
 // global variable for frams and paddle 
 const fps =60;
+const ballRadias = 8;
 const playerHeight=10;
 const playerWidth=100;
-let leftArrow = false;
-let rightArrow = false;
+let leftArrow = false;// for arror button
+let rightArrow = false;//for arror button 
 const ctx =canvas.getContext("2d");
-// to draw the paddle
-function drawrect(x,y,w,h,color){
-    ctx.fillStyle = color;
-    ctx.fillRect(x,y,w,h)
-}
 //opject of paddle 
 const player1 ={
         x:canvas.width/2-playerWidth/2,
-        y :canvas.height-10,
+        y :canvas.height-30,
         width:playerWidth,
         height:playerHeight,
         color:"blue",
         score:0,
         dx:5
-
     }
-
-
-
-
-
-
-
-
-
-    const ballRadias = 8;
-    // // ball attributes
+    // ball attributes
     const ball = {
         x: canvas.width,
         y: player1.y-ballRadias,
@@ -58,9 +40,7 @@ const player1 ={
     ball.dy = -2
     }
 
-
-
-    var count = 3;
+    var count = 3;//number of lifes
     // collision ball with wall
     function ballWallCollision() {
         if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
@@ -74,7 +54,6 @@ const player1 ={
             resetBall();// to return ball up paddle 
         }
     }
-
 // collision ball with paddle
 function ballPaddleCollision() {
     if (ball.y > player1.y && ball.y < player1.y + player1.height
@@ -88,8 +67,11 @@ function ballPaddleCollision() {
 
     }
 }
-
-
+// to draw the paddle
+function drawrect(x,y,w,h,color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x,y,w,h)
+}
     // // draw ball
     function drawBall() {
         ctx.beginPath();
@@ -103,14 +85,6 @@ function ballPaddleCollision() {
         ball.x += ball.dx
         ball.y += ball.dy
     }
-
-
-
-
-
-
-
-
     document.addEventListener('keydown', function (e) {
         if (e.keyCode == 37) {
             leftArrow = true
@@ -129,11 +103,9 @@ function ballPaddleCollision() {
         else if (e.keyCode == 39) {
             rightArrow = false;
             console.log("right")
-
-    
         }
     });
-    function movePaddle2() {
+    function movePaddle2() { //for paddle move with arrows
         if (rightArrow && player1.x + player1.width < canvas.width) {
             player1.x += player1.dx
     
@@ -144,33 +116,32 @@ function ballPaddleCollision() {
         }
     }
 
-
-
-
-
-    
+function movePaddle(){//for paddle move with mouse
+    canvas.addEventListener("mousemove",function(e){
+        let rect =canvas.getBoundingClientRect();
+        if (e.clientX-rect.left<97){}
+        else{
+        player1.x=e.clientX-rect.left-(player1.width);}   
+    })//height of player is 100  
+}
+     
     //to shows elemet in canvas  
     function rander(){
-        ctx.reset();// to delete canvas contant
-        drawBall()
-        drawrect(player1.x,player1.y,player1.width,player1.height,player1.color)// create new canvas by new position
-        }
+
+    ctx.reset();// to delete canvas contant
+    drawBall()// to create ball with new
+    drawrect(player1.x,player1.y,player1.width,player1.height,player1.color)// create new canvas by new position
+    }
     // rander()
     function update(){//to  update position of player(paddle)
-            canvas.addEventListener("mousemove",function(e){
-            let rect =canvas.getBoundingClientRect();
-            if (e.clientX-rect.left<97){}
-            else{
-            player1.x=e.clientX-rect.left-(player1.width);}    
-            movePaddle2()              
-            moveBall()
-            ballWallCollision()
-            ballPaddleCollision()
-
-
-
-        })//height of player is 100  
+        movePaddle()
+        movePaddle2()
+        moveBall()
+        ballWallCollision()
+        ballPaddleCollision()
     }
+    // movePaddle()
+    // movePaddle2()
     function game(){//to display frame by frame 
             update();
             rander();
@@ -178,79 +149,11 @@ function ballPaddleCollision() {
         setInterval(game,1000/fps)
 // =======
 
-// <<<<<<< HEAD
-// >>>>>>> 0f58650ad151e685c6691f0a627fe22fd93d2245
-// =======
-//Resizing Canvas
-// window.addEventListener('resize', resizeCanvas, false);
-// function resizeCanvas() {
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
-// }
-// resizeCanvas(); 
-// >>>>>>> e76386f379068f45cd5c719c82c7d094907653af
-
-
-
-    // // draw ball
-    function drawBall() {
-        ctx.beginPath();
-        ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2)
-        ctx.fillStyle = 'yellow';
-        ctx.fill()
-        ctx.closePath();
-    
-    }
-    function moveBall() {// change ball position
-        ball.x += ball.dx
-        ball.y += ball.dy
-    }
-
-
-
-
-
-
-
-
-
-
-
-    
-// // function movePaddle(){
-// //     canvas.addEventListener("mousemove",function(e){
-// //         let rect =canvas.getBoundingClientRect();
-// //         if (e.clientX-rect.left<97){}
-// //         else{
-// //         player1.x=e.clientX-rect.left-(player1.width);}   
-// //     })//height of player is 100  
-// // }
-     
-//     //to shows elemet in canvas  
-//     function rander(){
-
-//     ctx.reset();// to delete canvas contant
-//     drawBall()
-//     drawrect(player1.x,player1.y,player1.width,player1.height,player1.color)// create new canvas by new position
-//     }
-//     // rander()
-//     function update(){//to  update position of player(paddle)
-//         // movePaddle()
-//         movePaddle2()
-//         moveBall()
-//         ballWallCollision()
-//         ballPaddleCollision()
-//     }
-//     // movePaddle()
-//     // movePaddle2()
-//     function game(){//to display frame by frame 
-//             update();
-//             rander();
-//         }
-//         setInterval(game,1000/fps)
-// // =======
-
-
-
+window.addEventListener('resize', resizeCanvas, false);
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resizeCanvas();
 
 
